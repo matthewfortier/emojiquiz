ff54756ac0abc3422d5f706334b46b5ef44cc574
Morgan Seielstad <morgan.seielstad@mymail.champlain.edu>
2018-02-15 19:09:35 -0500

    Add files via upload
    Added comments

---
+++ EndGameViewController.swift
@@ -0,0 +1,67 @@
+//
+//  EndGameViewController.swift
+//  EmojiQuiz
+//
+//  Created by Matthew Fortier on 2/13/18.
+//  Copyright © 2018 Matthew Fortier. All rights reserved.
+//
+
+import UIKit
+
+class EndGameViewController: UIViewController {
+
+    @IBOutlet weak var Score: UILabel!
+    @IBOutlet weak var Name: UITextField!
+    
+    var sayings = [String: Int]()
+    var movies = [String: Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //display the score from the game
+        Score.text = String(UserDefaults.standard.integer(forKey: "score"))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //add the score to the highscores list in the user defaults
+    @IBAction func submitScore(_ sender: UIButton) {
+        if (UserDefaults.standard.integer(forKey: "category") == 0) {
+            
+            if (UserDefaults.standard.object(forKey: "movies") != nil) {
+                movies = UserDefaults.standard.dictionary(forKey: "movies") as! [String : Int]
+            }
+            
+            movies[Name.text!] = UserDefaults.standard.integer(forKey: "score")
+            
+            UserDefaults.standard.set(movies, forKey: "movies")
+        } else {
+            
+            if (UserDefaults.standard.object(forKey: "sayings") != nil) {
+                sayings = UserDefaults.standard.dictionary(forKey: "sayings") as! [String : Int]
+            }
+            
+            sayings[Name.text!] = UserDefaults.standard.integer(forKey: "score")
+            
+            UserDefaults.standard.set(sayings, forKey: "sayings")
+        }
+        
+        tabBarController?.selectedIndex = 1
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GameViewController.swift
@@ -0,0 +1,276 @@
+//
+//  GameViewController.swift
+//  EmojiQuiz
+//
+//  Created by Matthew Fortier on 2/8/18.
+//  Copyright © 2018 Matthew Fortier. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AudioToolbox
+
+extension String {
+    
+    //Enables replacement of the character at a specified position within a string
+    func replace(_ index: Int, _ newChar: Character) -> String {
+        var chars = Array(characters)
+        chars[index] = newChar
+        let modifiedString = String(chars)
+        return modifiedString
+    }
+    
+    func indexDistance(of character: Character) -> Int? {
+        guard let index = index(of: character) else { return nil }
+        return distance(from: startIndex, to: index)
+    }
+    
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)];
+    }
+}
+
+//From Dave Kopek, shuffle an array
+extension Array {
+    public func shuffled() -> Array<Element> {
+        var shuffledArray = self // value semantics (Array is Struct) makes this a copy
+        if count < 2 { return shuffledArray } // already shuffled
+        for i in (1..<count).reversed() { // count backwards
+            let position = Int(arc4random_uniform(UInt32(i + 1))) // random to swap
+            if i != position { // swap with the end, don't bother with self swaps
+                shuffledArray.swapAt(i, position)
+            }
+        }
+        return shuffledArray
+    }
+}
+
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var EmojiLabel: UILabel!
+    @IBOutlet weak var AnswerLabel: UILabel!
+    @IBOutlet weak var TimerLabel: UILabel!
+    
+    var currentAnswerString: String = ""
+    var currentQuestionIndex: Int = 0
+    
+    @IBOutlet weak var Keyboard: UIStackView!
+    
+    var questions: [Question] = [] //= [Question("🌽🐶", "Corn Dog", "Thing"), Question("🦁👑", "Lion King", "Thing")]
+    var numQuestions: Int = 0
+    
+    var category: String  = ""
+    var incorrectGuesses: Int = 0
+    var score: Int = 0
+    var countDown = 30
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    var timer: Timer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        numQuestions = UserDefaults.standard.integer(forKey: "numQuestions")
+        category = UserDefaults.standard.string(forKey: "category")!
+        readPlist()
+        setAnswersToDashes(0)
+        
+        EmojiLabel.text = questions[0].getQuestion()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCountDown), userInfo: nil, repeats: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //action for when any key on the keyboard is pressed
+    @IBAction func keyPress(_ sender: UIButton) {
+        let label: String = (sender.titleLabel?.text)!
+        
+        sender.isEnabled = false
+        sender.setTitleColor(.gray, for: UIControlState.normal)
+        
+        print(label)
+        
+        let uppercasedAnswer: String = questions[currentQuestionIndex].getAnswer().uppercased()
+        let currentAnswer: String = questions[currentQuestionIndex].getAnswer()
+        
+        //check the answer to see if the keyboard input is valid 
+        //if the input was valid update the string with the propper letter
+        if (uppercasedAnswer.contains(label)) {
+            
+            for i in 0..<currentAnswer.count {
+                
+                let index = currentAnswer.index(currentAnswer.startIndex, offsetBy: i)
+                
+                if uppercasedAnswer[index] == Character(label) {
+                    currentAnswerString = currentAnswerString.replace(i, currentAnswer[index])
+                }
+            }
+            //update the UI label
+            AnswerLabel.text = currentAnswerString
+            
+            //check states to move on
+            //if the current answer is complete and this is the last question jump to the leaderboard
+            if (!currentAnswerString.contains("-") && currentQuestionIndex == numQuestions - 1) {
+                score += 1
+                print("TEST")
+                playSound(wasSuccess: true)
+                loadLeaderBoard()
+                //if the current answer is complete move to the next question
+            } else if (!currentAnswerString.contains("-") && currentQuestionIndex < numQuestions) {
+                score += 1
+                playSound(wasSuccess: true)
+                loadNextQuestion()
+            }
+            //if the guess was incorrect play the sound, and set the key color to red.
+        } else {
+            incorrectGuesses += 1
+            sender.setTitleColor(.red, for: UIControlState.normal)
+            playSound(wasSuccess: false)
+            if incorrectGuesses == 3 {
+                loadLeaderBoard()
+            }
+        }
+    }
+    //update the timer label and check for timeout
+    @objc func updateCountDown() {
+        if(countDown > 0) {
+            TimerLabel.text = String(countDown)
+            countDown = countDown - 1
+        } else if (countDown == 0) {
+            countDown = countDown - 1
+            loadLeaderBoard()
+        }
+    }
+    
+    //set the label to all dashes and prepare the answer string to be edited later
+    func setAnswersToDashes(_ index: Int) {
+        let y = questions[index].getAnswer().replacingOccurrences(of: "[a-zA-Z0-9]", with: "-", options: .regularExpression, range: nil)
+        
+        currentAnswerString = y
+        AnswerLabel.text = y
+    }
+    
+    func loadNextQuestion() {
+        currentQuestionIndex += 1
+        countDown = 30
+    
+        setAnswersToDashes(currentQuestionIndex)
+        resetKeyboard()
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.EmojiLabel.alpha = CGFloat(0)
+        }, completion: {
+            (finished: Bool) -> Void in
+            
+            self.EmojiLabel.text = self.questions[self.currentQuestionIndex].getQuestion()
+            
+            UIView.animate(withDuration: 0.2, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+                self.EmojiLabel.alpha = CGFloat(1)
+            }, completion: nil)
+        })
+    }
+    
+    
+    
+    func loadLeaderBoard() {
+        
+        EmojiLabel.text = questions[0].getQuestion()
+        currentAnswerString = ""
+        currentQuestionIndex = 0
+        questions = questions.shuffled()
+        setAnswersToDashes(0)
+        resetKeyboard()
+        
+        UserDefaults.standard.set(score, forKey: "score")
+        performSegue(withIdentifier: "EndGame",
+                     sender: self)
+        
+        score = 0
+    }
+    
+    //check the value of the category switch to find the name of it
+    func checkCategory () {
+        
+        if category == "0" {
+            category = "movies"
+            print(category)
+        }
+        else if category == "1" {
+    
+            category = "sayings"
+            print(category)
+        }
+        else {
+            category = "ERROR"
+        }
+        
+    }
+    
+    //Use this function to play sounds, Pass it true for the success noise and flase for the fail noise
+    func playSound(wasSuccess: Bool) {
+        var path : String
+        if wasSuccess{
+            path = Bundle.main.path(forResource: "correct", ofType:"mp3")!
+            print("tada")
+        }
+        else {
+            path = Bundle.main.path(forResource: "wrong", ofType:"mp3")!
+            print("ouch")
+        }
+        let url = URL(fileURLWithPath: path)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            guard let audioPlayer = audioPlayer else { return }
+            
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+            print("SOUND!")
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    //reads from a file based on the category selected. shuffle the questions after loaded.
+    func readPlist () {
+        var newQuestion : Question
+        checkCategory()
+        if let path = Bundle.main.path(forResource: category, ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path) as? [String: Any] {
+                for item in 0..<Array(dict).count {
+                    newQuestion = Question(Array(dict)[item].key, String(describing: Array(dict)[item].value), "Movies")
+                    questions.append(newQuestion)
+                }
+            }
+        }
+        questions = questions.shuffled()
+    }
+    
+    
+    //re-enable all of the buttons on the keyboard
+    func resetKeyboard() {
+        for row: UIView in Keyboard.arrangedSubviews {
+            for col in row.subviews {
+                let button: UIButton = col as! UIButton
+                button.isEnabled = true
+                button.setTitleColor(UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1), for: UIControlState.normal)
+            }
+        }
+    }
+    
+    /*S
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
